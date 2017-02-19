975fe12a937dadda31a53f41559dd1b8f71c9877
Canyon <ravinsdrive@gmail.com>
2017-02-19 15:42:24 -0800

    Modified tournament

--- tournament.py
+++ tournament.py
@@ -227,16 +227,4 @@ def modified_main(weights_array):
     return weight_win
 
 if __name__ == "__main__":
-    import itertools
-    import pickle
-    own_moves = [3, 2, 1]
-    op_moves = [1, .8, .6, 0]
-    center_weights = [1, .6, .2, 0]
-    distance_weights = [1, .6, .2, 0]
-    weights = list(itertools.product(own_moves, op_moves, center_weights, distance_weights))
-    #weights = list(itertools.product(own_moves[:1], op_moves[:1], center_weights[:1], distance_weights[:1]))
-    #weights = list(itertools.product(own_moves[:2], op_moves[:2]))
-    print("Number of combinations: {0}".format(len(weights)))
-    weight_win = modified_main(weights)
-    with open("output.p", 'wb') as f:
-        pickle.dump(weight_win,f)
+    main()
